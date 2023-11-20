0b0627b293cfd35d5da171f04a82e5062101ff81
CiberNefty <dsveraa.ing@gmail.com>
2023-11-19 21:51:21 -0500

    Agregamos un primer archivo el cual entendemos sobre que son los espacios de trabajo, las primeras instrucciones y los saltos de linea en c++.

---
+++ Primeros pasos/01-holamundo/01-primerprograma.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Bibloteca
+using namespace std; // Esta line de codigo lo que estamos diciendo es que vamos a utilizar un estacion de nombres que se denomina std
+
+int main(){ 
+    cout << "Hola Mundo"; // esta linea sin la finalizacion (endl) es un salto de linea en c++
+    std::cout << " que buen ejemplo DVA" << std::endl;
+    
+    return 0;
+}
